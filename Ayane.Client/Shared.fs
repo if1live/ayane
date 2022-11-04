e59fcbb5db1803eb52ffc442b38e6bc8e6e26e49
if1live <libsora25@gmail.com>
2022-11-05 01:29:25 +0900

    cleanup package structure

--- Ayane.Client/Shared.fs
+++
@@ -1,9 +0,0 @@
-namespace Ayane.Shared
-
-open MagicOnion
-open System.Threading.Tasks
-
-type IMyFirstService =
-    inherit IService<IMyFirstService>
-
-    abstract member SumAsync: x: int -> y: int -> UnaryResult<int>
--- AyaneClient/Shared.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using MagicOnion;
-
-namespace AyaneShared;
-
-public interface IMyFirstService : IService<IMyFirstService>
-{
-    UnaryResult<int> SumAsync(int x, int y);
-}
