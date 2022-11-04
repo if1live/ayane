0e3981ce24bace1b60abde0ded039eb8a5822fc4
if1live <libsora25@gmail.com>
2022-11-05 00:01:49 +0900

    f# client

---
+++ Ayane.Client/Program.fs
@@ -0,0 +1,21 @@
+﻿open Grpc.Net.Client
+open MagicOnion.Client
+open Ayane.Shared
+
+let run (client: IMyFirstService) =
+    async {
+        let result = client.SumAsync 123 456
+        let! value = result.ResponseAsync |> Async.AwaitTask
+        printfn $"result: {value}"
+    }
+
+[<EntryPoint>]
+let main argv =
+    let channel = GrpcChannel.ForAddress("https://localhost:7253")
+    let client = MagicOnionClient.Create<IMyFirstService>(channel)
+
+    run client
+    |> Async.RunSynchronously
+    
+    0
+
