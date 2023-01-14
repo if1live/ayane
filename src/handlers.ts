dd94a81b14c83da1a0dbdb42ef0a6c74c6a2fdb2
if1live <libsora25@gmail.com>
2023-01-14 15:15:26 +0900

    error를 로직에서 직접 잡아서 처리하기
    에러 자체가 의미있게 취급되어야해서 최상위 핸들러에 의존하면 안된다

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,147 +2,51 @@ import * as dotenv from "@tinyhttp/dotenv";
 dotenv.config({ path: ".env.localhost" });
 
 import type { ScheduledHandler } from "aws-lambda";
-import { default as Redis } from "ioredis";
-import { default as pg } from "pg";
-import * as mysql from "mysql2/promise";
-import { RowDataPacket } from "mysql2";
+import {
+  touchMysqlSettled,
+  touchPostgresSettled,
+  touchRedisSettled,
+} from "./providers.js";
 
-const touch_planetscale = async () => {
+const execute_planetscale = async () => {
   const endpoint = process.env.MYSQL_DATABASE_URL;
   const label = "planetscale";
-
-  const execute = async () => {
-    if (!endpoint) {
-      console.log(label, "mysql endpoint is not defined");
-      return null;
-    }
-
-    const result = await touch_mysql(endpoint);
-    console.log(label, result);
-    return result;
-  };
-
-  return await wrapTouch(label, execute);
+  const result = await touchMysqlSettled(endpoint);
+  return { label, result };
 };
 
-const touch_supabase = async () => {
+const execute_supabase = async () => {
   const endpoint = process.env.POSTGRES_DATABASE_URL;
   const label = "supabase";
-
-  const execute = async () => {
-    if (!endpoint) {
-      console.log(label, "posgres endpoint is not defined");
-      return null;
-    }
-
-    const result = await touch_postgres(endpoint);
-    console.log(label, result);
-    return result;
-  };
-
-  return await wrapTouch(label, execute);
+  const result = await touchPostgresSettled(endpoint);
+  return { label, result };
 };
 
-const touch_redislab = async () => {
+const execute_redislab = async () => {
   const endpoint = process.env.REDIS_URL;
   const label = "redislab";
-
-  const execute = async () => {
-    if (!endpoint) {
-      console.log(label, "redis endpoint is not defined");
-      return null;
-    }
-
-    const result = await touch_redis(endpoint);
-    console.log(label, result);
-    return result;
-  };
-
-  return await wrapTouch(label, execute);
-};
-
-const wrapTouch = async (label: string, fn: () => Promise<unknown>) => {
-  try {
-    const value = await fn();
-    return { label, value };
-  } catch (e: any) {
-    e.label = label;
-    throw e;
-  }
+  const result = await touchRedisSettled(endpoint);
+  return { label, result };
 };
 
 export const touch: ScheduledHandler = async (event, context) => {
   console.log("event", JSON.stringify(event, null, 2));
 
-  const results = await Promise.allSettled([
-    touch_planetscale(),
-    touch_supabase(),
-    touch_redislab(),
+  const entries = await Promise.all([
+    execute_planetscale(),
+    execute_supabase(),
+    execute_redislab(),
   ]);
 
-  for (const result of results) {
-    if (result.status == "fulfilled") {
+  for (const entry of entries) {
+    const { label, result } = entry;
+    if (result.status === "fulfilled") {
       continue;
     }
 
     const reason = result.reason;
-    console.log(JSON.stringify(reason, null, 2));
-    console.log("");
+    console.log(label, reason);
   }
 
   // TODO: 처리 결과를 외부로 보여줄 필요가 있나? 리포트는 어디로 올리지?
-};
-
-const touch_mysql = async (endpoint: string) => {
-  const connection = await mysql.createConnection(endpoint);
-
-  const sql = "SELECT VERSION() AS version";
-  const result = await connection.execute(sql);
-  const rows = result[0] as RowDataPacket[];
-  const row = rows[0];
-  await connection.end();
-
-  const data = row as unknown;
-  const now = new Date();
-  return { ...(data as object), now };
-};
-
-const touch_postgres = async (endpoint: string) => {
-  const client = new pg.Client({
-    connectionString: endpoint,
-  });
-
-  await client.connect();
-  const result = await client.query(
-    "SELECT VERSION() AS version, NOW() AS now"
-  );
-  const row = result.rows[0];
-  await client.end();
-
-  const data = row as unknown;
-  return data;
-};
-
-const touch_redis = async (endpoint: string) => {
-  const url = new URL(endpoint);
-  const options: Redis.RedisOptions = {
-    host: url.hostname,
-    port: parseInt(url.port, 10),
-    password: url.password,
-    // https://github.com/luin/ioredis/issues/1123#issuecomment-920905876
-    enableAutoPipelining: true,
-    enableOfflineQueue: false,
-    lazyConnect: true,
-  };
-  // 컴파일은 되는데 타입이 망가진거같다? 그래서 생성자 캐스팅
-  const redis: Redis.Redis = new (Redis as any)(options);
-  await redis.connect();
-
-  const pong = await redis.ping();
-  const now = new Date();
-
-  redis.disconnect(false);
-
-  const data = { pong, now };
-  return data;
 };
---
+++ src/providers.ts
@@ -0,0 +1,120 @@
+import { default as Redis } from "ioredis";
+import { default as pg } from "pg";
+import * as mysql from "mysql2/promise";
+import { RowDataPacket } from "mysql2";
+
+/*
+설계 정책
+- connection은 제한된 자원이라서 touch 이후에 연결 해제
+- 외부 자원 연결은 실패할수 있고 이를 명시적으로 처리하고 싶다
+*/
+
+interface TouchFulfilledResult<T> {
+  status: "fulfilled";
+  value: T;
+}
+
+interface TouchRejectedResult {
+  status: "rejected";
+  reason: Error;
+}
+
+type TouchSettledResult<T> = TouchFulfilledResult<T> | TouchRejectedResult;
+
+export const wrapSettled = async <T>(
+  execute: () => Promise<T>
+): Promise<TouchSettledResult<T>> => {
+  try {
+    const value = await execute();
+    return { status: "fulfilled", value };
+  } catch (e: any) {
+    return { status: "rejected", reason: e as Error };
+  }
+};
+
+export const touchRedisSettled = async (
+  endpoint: string | undefined
+): Promise<TouchSettledResult<object | boolean>> => {
+  if (!endpoint) {
+    return { status: "fulfilled", value: false };
+  }
+
+  const execute = async () => await touchRedisNaive(endpoint);
+  return await wrapSettled(execute);
+};
+
+export const touchMysqlSettled = async (
+  endpoint: string | undefined
+): Promise<TouchSettledResult<object | boolean>> => {
+  if (!endpoint) {
+    return { status: "fulfilled", value: false };
+  }
+
+  const execute = async () => await touchMysqlNaive(endpoint);
+  return await wrapSettled(execute);
+};
+
+export const touchPostgresSettled = async (
+  endpoint: string | undefined
+): Promise<TouchSettledResult<object | boolean>> => {
+  if (!endpoint) {
+    return { status: "fulfilled", value: false };
+  }
+
+  const execute = async () => await touchPostgresNaive(endpoint);
+  return await wrapSettled(execute);
+};
+
+const touchRedisNaive = async (endpoint: string): Promise<object> => {
+  const url = new URL(endpoint);
+  const options: Redis.RedisOptions = {
+    host: url.hostname,
+    port: parseInt(url.port, 10),
+    password: url.password,
+    // https://github.com/luin/ioredis/issues/1123#issuecomment-920905876
+    enableAutoPipelining: true,
+    enableOfflineQueue: false,
+    lazyConnect: true,
+  };
+  // 컴파일은 되는데 타입이 망가진거같다? 그래서 생성자 캐스팅
+  const redis: Redis.Redis = new (Redis as any)(options);
+  await redis.connect();
+
+  const pong = await redis.ping();
+  const now = new Date();
+
+  redis.disconnect(false);
+
+  const data = { pong, now };
+  return data;
+};
+
+const touchMysqlNaive = async (endpoint: string): Promise<object> => {
+  const connection = await mysql.createConnection(endpoint);
+
+  const sql = "SELECT VERSION() AS version";
+  const result = await connection.execute(sql);
+  const rows = result[0] as RowDataPacket[];
+  const row = rows[0];
+  await connection.end();
+
+  const data = row as object;
+  const now = new Date();
+  return { ...data, now };
+};
+
+const touchPostgresNaive = async (endpoint: string): Promise<object> => {
+  const client = new pg.Client({
+    connectionString: endpoint,
+  });
+
+  await client.connect();
+  const result = await client.query(
+    "SELECT VERSION() AS version, NOW() AS now"
+  );
+  const row = result.rows[0];
+  await client.end();
+
+  const data = row as object;
+  return data;
+};
