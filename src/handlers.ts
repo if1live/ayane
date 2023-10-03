80e265f3ebd0ce0fa369844c1961b7eb794a3e05
if1live <libsora25@gmail.com>
2023-10-03 13:48:32 +0900

    sentry profiling 일단 비활성화
    번들링을 복잡하게 꼬아버리는 느낌

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,5 @@
 import type { APIGatewayProxyHandlerV2, ScheduledHandler } from "aws-lambda";
 import * as Sentry from "@sentry/serverless";
-import { ProfilingIntegration } from "@sentry/profiling-node";
 import { app } from "./app.js";
 import { handle } from "hono/aws-lambda";
 import { touch } from "./services.js";
@@ -9,15 +8,10 @@ import { SENTRY_DSN, NODE_ENV } from "./settings.js";
 if (SENTRY_DSN) {
   Sentry.AWSLambda.init({
     dsn: SENTRY_DSN,
-    integrations: [new ProfilingIntegration()],
 
     // Performance Monitoring
     // Capture 100% of the transactions, reduce in production!
     tracesSampleRate: NODE_ENV === "production" ? 0.1 : 1.0,
-
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    // Capture 100% of the transactions, reduce in production!
-    profilesSampleRate: NODE_ENV === "production" ? 0.1 : 1.0,
 
     // 커밋할때 확인하기
     // debug: true,
